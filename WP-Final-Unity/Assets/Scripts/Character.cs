638fd3b614adcf23250587021afbfb9e63722e5a
1Needle <tmpss99170@gmail.com>
2023-12-21 04:08:13 +0800

    Finish skeleton enemy
    -Add 3 scripts
    -update Character scripts
    -prefab can be find in Asset/Character/Skeleton

--- WP-Final-Unity/Assets/Scripts/Character.cs
+++ WP-Final-Unity/Assets/Scripts/Character.cs
@@ -5,40 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(Collider))]
 [RequireComponent(typeof(Animator))]
-public class Character : MonoBehaviour
+public abstract class Character : MonoBehaviour
 {
-    // components
-    Rigidbody rb;
-    Animator animator;
-    Collider collider;
-    // variables
-    [SerializeField] float maxHealth;
-    [SerializeField] float walkSpeed;
-    [SerializeField] float runSpeed;
-    [SerializeField] float speed;
-    float health;
-    bool alive;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
-        collider = GetComponent<Collider>();
-        health = maxHealth;
-        alive = true;
-    }
-
     // functions
-    public void Move()
-    {
-        
-    }
-    public void Attack()
-    {
-
-    }
-    public void Hurt()
-    {
-
-    }
+    public abstract void Hurt(float damage);
 }
