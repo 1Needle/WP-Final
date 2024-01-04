c7c4eae90d1ac0365fdbd00ffa2721a3c32a0d47
luckofdraws <145518895+luckofdraws@users.noreply.github.com>
2024-01-04 20:43:17 +0800

    Player
    +attack judging

--- WP-Final-Unity/Assets/Characters/Player/Scripts/SwordCollider.cs
+++ WP-Final-Unity/Assets/Characters/Player/Scripts/SwordCollider.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
 public class SwordCollider : MonoBehaviour
 {
+    [SerializeField] GameObject Particle_StarHit;
+    [SerializeField] GameObject Particle_StoneHit;
+    [SerializeField] Transform This;
+
     [SerializeField] PlayerController playerController;
     [SerializeField] CapsuleCollider capsuleCollider;
     [SerializeField] PlayerData playerData;
@@ -30,12 +35,80 @@ public class SwordCollider : MonoBehaviour
             SwordCollision = false;
             capsuleCollider.enabled = false;
         }
-        if(Attacking == true && SwordCollision == false)
+        if(Attacking == true && SwordCollision == false && thisAttacking == false)
         {
-            if(capsuleCollider.enabled == false)
-            {
-                capsuleCollider.enabled = true;
-            }
+            thisAttacking = true;
+            Delay_On = true;
+            Collider_Closer = true;
+        }
+
+        if(Delay_On == true)
+        {
+            Delay_SwordCollider();
+        }
+        if(Collider_Closer == true)
+        {
+            Collider_Closer_SwordCollider();
+        }
+        if (thisAttacking == true)
+        {
+            thisAttacking_SwordCollider();
+        }
+    }
+
+    private bool Delay_On = false;
+    private float Delay_timer = 0f;  // �p�ɾ��ܼ�
+    private float Delay_duration = 0.1f;  // �p�ɪ��`�ɶ�
+    private void Delay_SwordCollider()
+    {
+        // ��s�p�ɾ�
+        Delay_timer += Time.deltaTime;
+
+        // �ˬd�O�_�W�L�F���w���ɶ�
+        if (Delay_timer >= Delay_duration)
+        {
+            capsuleCollider.enabled = true;
+            Delay_On = false;
+
+            // ���m�p�ɾ�
+            Delay_timer = 0f;
+        }
+    }
+
+    private bool Collider_Closer = false;
+    private float Collider_Closer_timer = 0f;  // �p�ɾ��ܼ�
+    private float Collider_Closer_duration = 0.4f;  // �p�ɪ��`�ɶ�
+    private void Collider_Closer_SwordCollider()
+    {
+        // ��s�p�ɾ�
+        Collider_Closer_timer += Time.deltaTime;
+
+        // �ˬd�O�_�W�L�F���w���ɶ�
+        if (Collider_Closer_timer >= Collider_Closer_duration)
+        {
+            capsuleCollider.enabled = false;
+            Collider_Closer = false;
+
+            // ���m�p�ɾ�
+            Collider_Closer_timer = 0f;
+        }
+    }
+
+    private bool thisAttacking = false;
+    private float thisAttacking_timer = 0f;  // �p�ɾ��ܼ�
+    private float thisAttacking_duration = 1f;  // �p�ɪ��`�ɶ�
+    private void thisAttacking_SwordCollider()
+    {
+        // ��s�p�ɾ�
+        thisAttacking_timer += Time.deltaTime;
+
+        // �ˬd�O�_�W�L�F���w���ɶ�
+        if (thisAttacking_timer >= thisAttacking_duration)
+        {
+            thisAttacking = false;
+
+            // ���m�p�ɾ�
+            thisAttacking_timer = 0f;
         }
     }
 
@@ -55,7 +128,7 @@ public class SwordCollider : MonoBehaviour
 
             if (playerController.Get_OnFire() == true)
             {
-                character.Fire_Hurt(dmg, 3);
+                character.Fire_Hurt(Fire_dmg, 3);
             }
 
                 //Debug.Log($"Player->Enemy: {dmg}");
@@ -68,7 +141,11 @@ public class SwordCollider : MonoBehaviour
                 {
                     character.Hurt(dmg);
                 }
+
+
             
+            Instantiate(Particle_StarHit, This.position, Quaternion.identity);
+            Instantiate(Particle_StoneHit, This.position, Quaternion.identity);
 
             //close collider to avoid multi-collision
             capsuleCollider.enabled = false;
